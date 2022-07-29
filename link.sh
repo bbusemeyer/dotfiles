37904e55bf12a926df816112575cfc2feb037bed
Brian Busemeyer <bbusemeyer@gmail.com>
2022-07-29 18:13:04 -0400

    new file:   config/mydefault.vim 	new file:   config/mydelek.vim 	new file:   config/mydesert.vim 	modified:   link.sh 	modified:   purge.sh

--- link.sh
+++ link.sh
@@ -5,6 +5,7 @@ bash_prefix='base'
 wd=`pwd`
 ln -vs ${wd}/config/${bash_prefix}_bashrc ${HOME}/.bashrc
 ln -vs ${wd}/config/${bash_prefix}_aliases ${HOME}/.bash_aliases
+ln -vs ${wd}/config/mydelek.vim ${HOME}/.vim/colors
 for fn in bash_profile vimrc inputrc gitconfig gitignore_global
 do
   ln -vs ${wd}/config/${fn} ${HOME}/.${fn}
