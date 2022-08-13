09ad8247887bd851ac2b85c747a7bbcee289d0f7
Brian Busemeyer <bbusemeyer@gmail.com>
2022-08-13 15:54:54 -0400

    Reorg vim

--- setup/install_neovim.sh
+++ setup/install_neovim.sh
@@ -5,5 +5,6 @@ git checkout release-0.7
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 mkdir install
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=`pwd`/install
+make install
 cd $cwd
 
