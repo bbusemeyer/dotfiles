0d48fab0cfbe2d6a8d58b9be408bb24df6b5e290
Brian Busemeyer <bbusemeyer@gmail.com>
2022-08-13 07:44:16 -0400

    new file:   install_neovim.sh

---
+++ setup/install_neovim.sh
@@ -0,0 +1,9 @@
+cwd=`pwd`
+cd ${HOME}/soft
+git clone https://github.com/neovim/neovim
+git checkout release-0.7
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+mkdir install
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=`pwd`/install
+cd $cwd
+
