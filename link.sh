5d00cc351d5250b4d28650179ac2b4db9606cb80
Brian <bbusemeyer@gmail.com>
2022-08-24 17:19:32 -0400

    More organization, added zshrc from Niel's page.

--- link.sh
+++ link.sh
@@ -1,12 +1,6 @@
 
-# Edit this to specialize the Bash environment to a specific computer.
-bash_prefix='base'
-
 wd=`pwd`
-ln -vs ${wd}/config/${bash_prefix}_bashrc ${HOME}/.bashrc
-ln -vs ${wd}/config/${bash_prefix}_aliases ${HOME}/.bash_aliases
-ln -vs ${wd}/config/mydelek.vim ${HOME}/.vim/colors
-for fn in bash_profile vimrc inputrc gitconfig gitignore_global
+for fn in gitignore gitconfig_global shrc zshrc oh-my-zsh
 do
   ln -vs ${wd}/config/${fn} ${HOME}/.${fn}
 done
