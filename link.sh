4b1bb776964ea40f403ce21e7d95b3b1847d6654
Brian <bbusemeyer@gmail.com>
2022-08-24 17:38:41 -0400

    Fixed linking script.

--- link.sh
+++
@@ -1,6 +0,0 @@
-
-wd=`pwd`
-for fn in gitignore gitconfig_global shrc zshrc oh-my-zsh
-do
-  ln -vs ${wd}/config/${fn} ${HOME}/.${fn}
-done
---
+++ setup/link.sh
@@ -0,0 +1,10 @@
+# Link the configuration files in the places they should go.
+
+cwd=`pwd`
+cd ../config
+wd=`pwd`
+for fn in gitignore gitconfig_global shrc zshrc oh-my-zsh
+do
+  ln -vs ${wd}/${fn} ${HOME}/.${fn}
+done
+cd $cwd
