f25066bf417a2b4239114e81c1060554e7ba1547
Brian Busemeyer <bbusemeyer@gmail.com>
2024-04-06 13:48:59 -0400

    Added vim folder linking

--- clean_links.sh
+++ clean_links.sh
@@ -2,6 +2,7 @@ wd=`pwd`
 rm -v  ${HOME}/.shrc
 rm -v  ${HOME}/.bashrc
 rm -v  ${HOME}/.config/nvim/init.vim
+rm -v  ${HOME}/.vim/*
 for fn in bash_profile vimrc inputrc gitconfig gitignore_global
 do
   rm -v ${HOME}/.${fn}
--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@ wd=`pwd`
 ln -vs ${wd}/config/shrc ${HOME}/.shrc
 ln -vs ${wd}/config/bashrc ${HOME}/.bashrc
 ln -vs ${wd}/config/init.vim ${HOME}/.config/nvim/init.vim
+ln -vs ${wd}/config/dotvim/* ${HOME}/.vim
 for fn in bash_profile vimrc inputrc gitconfig gitignore_global
 do
   ln -vs ${wd}/config/${fn} ${HOME}/.${fn}
