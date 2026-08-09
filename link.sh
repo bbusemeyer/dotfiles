1c3d7ea19aea270bcb9869359624bc7aad818ca6
Brian Busemeyer <bbusemeyer@gmail.com>
2026-08-08 20:26:32 -0400

    Fixing install script

--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 
 wd=`pwd`
-ln -vs ${wd}/config/shrc ${HOME}/.shrc
-ln -vs ${wd}/config/bashrc ${HOME}/.bashrc
-ln -vs ${wd}/config/init.vim ${HOME}/.config/nvim/init.vim
-ln -vs ${wd}/config/dotvim/* ${HOME}/.vim
-ln -vs ${wd}/config/flake8 ${HOME}/.config/flake8
+ln -fvs ${wd}/config/shrc ${HOME}/.shrc
+ln -fvs ${wd}/config/bashrc ${HOME}/.bashrc
+ln -fvs ${wd}/config/init.vim ${HOME}/.config/nvim/init.vim
+ln -fvs ${wd}/config/dotvim/* ${HOME}/.vim
+ln -fvs ${wd}/config/flake8 ${HOME}/.config/flake8
 for fn in bash_profile vimrc inputrc gitconfig gitignore_global
 do
-  ln -vs ${wd}/config/${fn} ${HOME}/.${fn}
+  ln -fvs ${wd}/config/${fn} ${HOME}/.${fn}
 done
--- setup/install_neovim.sh
+++ setup/install_neovim.sh
@@ -1,7 +1,6 @@
 # neovim that ships with Ubuntu is insanely old. Need to use special install to do it. 
 
-wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-tar xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-mv nvim-linux64 ~/soft
-ln -s ~/soft/nvim-linux64/bin/nvim ~/.local/bin
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+ln -fs /opt/nvim-linux-x86_64/bin/nvim ~/.local/bin
